1de5b9c7d1a10b9bc8e71f1adada6dcb991ac76d
Higgins2222 <131566242+Higgins2222@users.noreply.github.com>
2024-12-28 08:03:18 -0500

    Version .07
    Many changes, order of operations is now merged with the main display and only one compute path is taken.

--- tkinter_calc.py
+++ tkinter_calc.py
@@ -1,6 +1,62 @@
-# order of operations is done correctly on the 'equation' display, and the textbox display still works like the originial in the tutorial that does not process order of operations.
+# Build 6
+# self.display has been removed entirely
 
-# pressing '=' now resets the equation
+# calculate_result_inline has been removed as we no longer calculate the result as we are entering an equation, as we are now always respecting order of opperations
+
+#...
+#added equation class instead of equation global var
+
+#the equation now clears on pressing clear_display, no longer calling clear_display for each clear, only when the clear button is pressed
+
+# FIXED as of version 5
+#### bug, errors present when float is introduced by division
+
+# Some error handeling has been added for divide by 0
+
+# removed all but one debugging print statement
+
+# we now use a self.label member variable and the label.config method to update the label box dispalying the equation
+
+# perform_operation has now been added to the CalculatorApp class to simplify repeated text inside the opperation button functions
+
+# the self.label member variable now shows the full equation and the result when pressing '='
+
+# the self.label member variable now properly shows as empty when clear is pressed
+
+# removed unused variables including self.current_operation
+
+# removed stray comments
+
+# added a decimal point button
+
+# added an exponent button
+
+# added a square root button
+
+# added a negative sign button
+
+
+### todo ###
+# clean up methods exponent, mult_div, add_sub and solver.  Maybe use lambdas to avoid repeated code, either add to equation class or create own class
+
+# add error checking to make sure equation is well formed
+
+# make keyboard number buttons work
+    # add a way to hide the number buttons
+
+# add a history
+
+# add parentheses buttons
+
+# add memory buttons
+
+# add a clear current number button
+
+# error display for things like 5, +, *, 3, or 5, 5, +, 5
+
+# add back in the entry box and allow a full equation to be input at once (copy paste)
+
+# UI improvements, make it look nice
 
 import tkinter as tk
 
@@ -35,6 +91,8 @@ def mult_div(equation):
         elif token == '/':
             left = temp.pop()
             right = equation.pop(0)
+            if right == 0:
+                    raise ValueError("Division by zero")
             result = left / right
             temp.append(result)
         else:
@@ -87,22 +145,48 @@ def solver(equation):
 
     return temp
 
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
 
+class Equation:
+    def __init__(self):
+        self.equation = []
 
-equation = []
+    def append_term_and_opp(self, num, operation):
+        # todo: if num.isdigit():
+        self.equation.append(str(num))
+        # todo: if operation in list_of_opps:
+        self.equation.append(operation)
+        #print(equation)
+
+    def append_term(self, num):
+        # note: this will commonly be called as '=' is pressed
+        # todo: if num.isdigit():
+        # todo: if equation[-1] in list_of_opps
+        self.equation.append(str(num))
+
+    def clear(self):
+        self.equation = []
+
+    def get_solution(self):
+        print(self.equation)
+        equation_int = [float(x) if is_number(x) else x for x in self.equation]
+        return solver(equation_int)[0]
+
 
 class CalculatorApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Simple Calculator")
         
-        self.current_operation = None
-        self.first_number = None
-        self.second_number = None
 
-        # Entry widget for display
-        self.display = tk.Entry(root, width=35, borderwidth=5)
-        self.display.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+        self.current_number = []
+
+        self.equation = Equation()
 
         # Creating the buttons
         self.create_buttons()
@@ -129,98 +213,77 @@ class CalculatorApp:
         tk.Button(self.root, text='Clear', padx=79, pady=20, command=self.clear_display).grid(row=4, column=1, columnspan=2)
         tk.Button(self.root, text='=', padx=91, pady=20, command=self.calculate_result).grid(row=6, column=1, columnspan=2)
 
+        tk.Button(self.root, text='.', padx=38, pady=20, command=self.point_click).grid(row=7, column=0)
 
-        equation_label = tk.Label(root, width=35, borderwidth=5)
-        equation_label.grid(row=7, column=0, columnspan=3, padx=10, pady=10)
+        tk.Button(self.root, text='^', padx=38, pady=20, command=self.button_exponent).grid(row=7, column=1)
 
-    def append_to_equation(self, first_number, operation):
-        equation.append(str(first_number))
-        equation.append(operation)
-        print(equation)
-        equation_label = tk.Label(root, text=' '.join(equation), width=35, borderwidth=5)
-        equation_label.grid(row=7, column=0, columnspan=3, padx=10, pady=10)
+        tk.Button(self.root, text='-', padx=38, pady=20, command=self.negative_click).grid(row=7, column=2)
 
+        self.equation_label = tk.Label(root, width=35, borderwidth=5)
+        self.equation_label.grid(row=8, column=0, columnspan=3, padx=10, pady=10)
+
+    def point_click(self):
+        self.current_number.append('.')
+        self.equation_label.config(text=f'{" ".join(self.equation.equation)} {"".join(self.current_number)}')
+
+    def negative_click(self):
+        self.current_number.insert(0, '-')
+        self.equation_label.config(text=f'{" ".join(self.equation.equation)} {"".join(self.current_number)}')
 
     def button_click(self, number):
         """Appends a digit to the display."""
-        current = self.display.get()
-        self.display.delete(0, tk.END)
-        self.display.insert(0, current + str(number))
+        self.current_number.append(str(number))
+        self.equation_label.config(text=f'{" ".join(self.equation.equation)} {"".join(self.current_number)}')
 
     def clear_display(self):
         """Clears the display."""
-        self.display.delete(0, tk.END)
+        self.equation.clear()
+        self.equation_label.config(text=' '.join(self.equation.equation))
+        self.current_number = []
+
+    def set_operation(self, symbol, operation):
+        """Sets the current operation and stores the first number."""
+        # The operation variable is not currently used, but may be useful later to keep track of the names of more complex opperations where the symbol could be obscure or ambiguous
+
+        num = float("".join(self.current_number))
+        self.equation.append_term_and_opp(num, symbol)
+        self.current_number = []
+        self.equation_label.config(text=' '.join(self.equation.equation))
 
     def button_add(self):
-        self.append_to_equation(int(self.display.get()), '+')
-        self.set_operation('addition')
-        
+        self.set_operation('+', 'addition')
 
     def button_subtract(self):
-        self.append_to_equation(int(self.display.get()), '-')
-        self.set_operation('subtraction')
-        
+        self.set_operation('-', 'subtraction')
 
     def button_multiply(self):
-        self.append_to_equation(int(self.display.get()), '*')
-        self.set_operation('multiplication')
-        
+        self.set_operation('*', 'multiplication')
 
     def button_divide(self):
-        self.append_to_equation(int(self.display.get()), '/')
-        self.set_operation('division')
-        
+        self.set_operation('/', 'division')
 
-    def set_operation(self, operation):
-        """Sets the current operation and stores the first number."""
-        print(f'{self.first_number=} {self.second_number=}')
-        if self.first_number:
-            self.calculate_result_inline()        
-        self.first_number = int(self.display.get())
-        self.current_operation = operation
-
-        self.clear_display()
-
-    def calculate_result_inline(self):
-        """Performs the selected operation and displays the result."""
-        second_number = int(self.display.get())
-        result = None
-
-        if self.current_operation == 'addition':
-            result = self.first_number + second_number
-        elif self.current_operation == 'subtraction':
-            result = self.first_number - second_number
-        elif self.current_operation == 'multiplication':
-            result = self.first_number * second_number
-        elif self.current_operation == 'division':
-            if second_number == 0:
-                result = "Error"  # Handling division by zero
-            else:
-                result = self.first_number / second_number
-
-        self.clear_display()
-        self.first_number = None
-        self.current_operation = None
-        if result is not None:
-            self.display.insert(0, str(result))
+    def button_exponent(self):
+        self.set_operation('**', 'exponent')
+        #self.current_number.append('**')
+        #self.equation_label.config(text=f'{" ".join(self.equation.equation)} {"".join(self.current_number)}')
 
     def calculate_result(self):
-        global equation
         """Performs the selected operation and displays the result."""
-        second_number = int(self.display.get())
+        print(self.current_number)
+        num = float("".join(self.current_number))
+        self.equation.append_term(str(num))
+
         result = None
+        try:
+            result = self.equation.get_solution()
+            self.equation_label.config(text=f'{" ".join(self.equation.equation)} = {str(result)}')
+        except Exception as e:
+            self.equation_label.config(text=f'Error {e}')
 
-        self.calculate_result_inline()
-
-        equation.append(str(second_number))
-
-        equation_int = [float(x) if x.isdigit() else x for x in equation]
-
-        result = solver(equation_int)
-        equation_label = tk.Label(root, text=str(result), width=35, borderwidth=5)
-        equation_label.grid(row=7, column=0, columnspan=3, padx=10, pady=10)
-        equation = []
-
+        self.current_operation = None
+        self.current_number = list(str(result))
+        print(self.current_number)
+        self.equation.clear()
 
 
 
